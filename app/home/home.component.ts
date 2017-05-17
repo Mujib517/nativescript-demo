686a6a9d078576230064056720e39d6d53ea3df8
rahmam <mohammed.rahman@cdk.com>
2017-05-17 15:54:20 +0530

    Tabs demo

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     template: `
@@ -6,8 +7,36 @@ import { Component } from '@angular/core';
     <ActionBar title="Task Manager" class="action-bar">
     </ActionBar>
 
-    <Button class="btn" text="Go To Tasks" [nsRouterLink]="['/tasks']"></Button>
+<Button dock="bottom" class="btn" text="Go To Tasks" [nsRouterLink]="['/tasks']"></Button>
+
+    <StackLayout>
+            <TabView #tabview dock="bottom">
+            <StackLayout *tabItem="{title: 'Tab1'}">
+                <Label text="This is Label in Tab 1"></Label>
+            </StackLayout>
+            <StackLayout *tabItem="{title: 'Tab2'}" (tap)="onTap()" [nsRouterLink]="['/tasks']">
+                <Label text="This is Label in Tab 2"></Label>
+            </StackLayout>
+            <StackLayout *tabItem="{title: 'Tab3'}">
+                <Label text="This is Label in Tab 3"></Label>
+            </StackLayout>
+            </TabView>
+
+            
+    </StackLayout>  
+
+
     `
 })
 
-export class HomeComponent { }
+export class HomeComponent {
+
+    constructor(private router: Router) {
+
+    }
+
+    onTap() {
+        console.log('htting');
+        this.router.navigate(['/tasks']);
+    }
+}
